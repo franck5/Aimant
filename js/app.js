73021aecccf894b3854d06b4311f0cfdc2ef24e2
dubech <franck.dubech@hotmail.com>
2017-03-28 20:14:25 +0200

    inintial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+function generatepoint(number){
+	for(var i = 0; i < number; i++){
+	$("<div/>").addClass('debris').appendTo('#jeu');
+	//$('debris').offset(top: 100, left: 300);
+
+}	
+
+}
+
+	generatepoint(100);
+
+});
+
+ 
